0e4cfc6121d5800d47713c677ac887de4cd28233
Conexão Street <98308373+ConexaoStreet@users.noreply.github.com>
2026-05-29 20:37:50 -0300

    Ajusta tom do site para ficar mais natural

---
+++ photos.js
@@ -0,0 +1,8 @@
+// Cadastre aqui os caminhos das fotos que você colocar em assets/fotos/.
+// O GitHub Pages não lista arquivos automaticamente, então adicione cada imagem neste array.
+// Formatos aceitos: .jpg, .jpeg, .png e .webp (desde que o caminho esteja correto).
+const COUPLE_PHOTOS = [
+  "./assets/fotos/foto1.jpg",
+  "./assets/fotos/foto2.jpg",
+  "./assets/fotos/foto3.jpg"
+];
